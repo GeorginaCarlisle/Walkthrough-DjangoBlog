e1b415b62fca78909e4c8bdc3e70a01805988ef2
Georgina Carlisle <georgina.carlisle@gmail.com>
2023-11-04 21:13:14 +0000

    Edit allowed hosts

--- codestar2021/settings.py
+++ codestar2021/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'walkthrough-django-blog.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'walkthrough-django-blog-ad4b634bed46.herokuapp.com', 'localhost']
 
 
 # Application definition
