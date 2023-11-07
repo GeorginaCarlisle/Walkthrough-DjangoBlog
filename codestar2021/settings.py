9e6ff621437d3ca95b309585bff1554d5b1b4980
Georgina Carlisle <georgina.carlisle@gmail.com>
2023-11-07 20:07:52 +0000

    Deployment commit

--- codestar2021/settings.py
+++ codestar2021/settings.py
@@ -30,7 +30,9 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+
+X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 ALLOWED_HOSTS = ['127.0.0.1', 'walkthrough-django-blog-ad4b634bed46.herokuapp.com', 'localhost']
 
